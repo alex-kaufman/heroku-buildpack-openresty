48eeaa9460642c2e00d072b86cc8f6fce5ed6625
alex-kaufman <alex@stashinvest.com>
2018-01-17 15:28:00 -0500

    update resolver

--- .profile.d/resolver.sh
+++ .profile.d/resolver.sh
@@ -1,4 +1,3 @@
-
+#!/usr/bin/env bash
 # get IP address of dns resolver
-# RESOLVER=grep '^nameserver' resolv.conf | awk '{print $2}'
-export RESOLVER=$(grep '^nameserver' resolv.conf | awk '{print $2}')
+RESOLVER=$(grep '^nameserver' resolv.conf | awk '{print $2}')
