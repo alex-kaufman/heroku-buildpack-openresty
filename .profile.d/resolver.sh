e633cb067aad09341c37b3f51c924fab992ae549
alex-kaufman <alex@stashinvest.com>
2018-01-17 15:54:17 -0500

    add echoes

--- .profile.d/resolver.sh
+++ .profile.d/resolver.sh
@@ -2,3 +2,6 @@
 # get IP address of dns resolver
 RESOLVER=$(grep '^nameserver' resolv.conf | awk '{print $2}')
 export RES=$(grep '^nameserver' resolv.conf | awk '{print $2}')
+
+echo $RESOLVER
+echo $RES
